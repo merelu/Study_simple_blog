f1f7fab47d69d1ed421d51a2b566c3b25affae92
GYUHA_HWANG <gyuha0310@gmail.com>
2020-12-07 22:16:00 +0900

    index.js작성 및 user.js 관계 설정

--- models/user.js
+++ models/user.js
@@ -40,5 +40,17 @@ module.exports = class User extends (
     );
   }
 
-  static associate(db) {}
+  static associate(db) {
+    db.User.hasMany(db.Post);
+    db.User.belongsToMany(db.User, {
+      foreignKey: "followingId",
+      as: "Followers",
+      through: "Follow",
+    });
+    db.User.belongsToMany(db.User, {
+      foreignKey: "followerId",
+      as: "Followings",
+      through: "Follow",
+    });
+  }
 };
