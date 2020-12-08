3700c3d56e6c9faa078d2eac67d6e343dec3ed5a
GYUHA_HWANG <gyuha0310@gmail.com>
2020-12-08 21:54:30 +0900

    app.js에 passport를 사용하기 위한 셋팅

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const path = require("path");
 const session = require("express-session");
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
-const { sequelize } = require("./models");
+const passport = require("passport");
 dotenv.config();
 const pageRouter = require("./routes/page");
+const { sequelize } = require("./models");
+const passportConfig = require("./passport");
 
 const app = express();
+passportConfig();
+
 app.set("port", process.env.PORT || 8001);
 app.set("view engine", "html");
 nunjucks.configure("views", {
@@ -41,6 +45,8 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
 app.use("/", pageRouter);
 
 app.use((req, res, next) => {
